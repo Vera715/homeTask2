30f8a7362aa252256d770b745ce675fb5bf3a85f
Vera711 <grishayeva.vera@list.ru>
2025-03-08 20:48:16 +0300

    homeTask2

---
+++ homeTask2_random_.ino
@@ -0,0 +1,28 @@
+#include <FastLED.h>
+#define NUM_LEDS 54
+#define DATA_PIN 9
+CRGB leds[NUM_LEDS];
+void setup() {
+  Serial.begin(9600);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+}
+
+void loop() {
+  for (int i = 0; i < 54; i++ )
+  {
+    int start = 0;
+    int end = 255;
+    int r = rand() % (end - start + 1) + start;
+    int g = rand() % (end - start + 1) + start;
+    int b = rand() % (end - start + 1) + start;
+    leds[i] = CRGB(r, g, b);
+    FastLED.show();
+    delay(150);
+  }
+  for (int i = 54; i >= 0; i--)
+  {
+    leds[i] = CRGB(0, 0, 0);
+    FastLED.show();
+    delay(150);
+  }
+}
